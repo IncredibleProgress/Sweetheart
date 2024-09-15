f819b828e0b9429385146084b15718da0322df71
Nicolas Champion <66551234+IncredibleProgress@users.noreply.github.com>
2024-09-15 15:13:10 +0000

    new init.bash script

--- configuration/init.bash
+++ configuration/init.bash
@@ -6,30 +6,31 @@ printf "\033[0;31m%s\033[0m\n" \
 
 # manage system update
 reload="disabled"
-sudo apt-get update -q && sudo apt-get upgrade -q -y
+sudo apt-get update -qq && sudo apt-get upgrade -q -y
 
 # get current ubuntu codename and python version
 codename=$(grep -w UBUNTU_CODENAME /etc/os-release | cut -d'=' -f2 | tr -d '"')
 version=$(python3 --version | awk '{print $2}' | cut -d. -f1,2)
-
-# set default values for Nginx Unit
-unit_codename="$codename"
 unit_python="unit-python$version"
 
 if [[ -z "$codename" ]]; then
   echo "ERR: Ubuntu is required as OS for Sweetheart"
   exit 1
 elif [[ "$codename" == "noble" ]]; then
-  #FIXME: waiting for updates in official Nginx Unit repositories
-  # lunar/python3.11 are set here instead of noble/python3.12
-  unit_codename="mantic"
+  #FIXME: waiting for updates in official repositories
+  # mantic is set here instead of noble for RethinkDB and Nginx Unit
+  codename="mantic"
   # unit_python="libpython3.11 unit-python3.11"
   # # set repository for libpython3.11
   # sudo add-apt-repository --yes ppa:deadsnakes/ppa
 fi
 
-# set official RethinkDB repository 
-if [[ -z "$(apt-cache policy rethinkdb)" ]]; then
+# get candidates for RethinkDB and Nginx Unit
+redb_candidate=$(apt-cache policy rethinkdb | awk '/Candidate/ {print $2}')
+unit_candidate=$(apt-cache policy unit | awk '/Candidate/ {print $2}')
+
+# set official RethinkDB repository
+if [[ -z "$redb_candidate" || "$redb_candidate" == "(none)" ]]; then
 
     wget -qO- https://download.rethinkdb.com/repository/raw/pubkey.gpg \
     | sudo gpg --dearmor -o /usr/share/keyrings/rethinkdb-archive-keyrings.gpg
@@ -39,11 +40,12 @@ if [[ -z "$(apt-cache policy rethinkdb)" ]]; then
       "https://download.rethinkdb.com/repository/ubuntu-$codename $codename main"\
     | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 
+    # Mark to reload the package list
     reload="enabled"
 fi
 
 # set official Nginx Unit repository 
-if [[ -z "$(apt-cache policy unit)" ]]; then
+if [[ -z "$unit_candidate" || "$unit_candidate" == "(none)" ]]; then
 
     wget -qO- https://unit.nginx.org/keys/nginx-keyring.gpg \
     | sudo gpg --dearmor -o /usr/share/keyrings/nginx-keyring.gpg
@@ -53,10 +55,11 @@ if [[ -z "$(apt-cache policy unit)" ]]; then
       "https://packages.nginx.org/unit/ubuntu/ $unit_codename unit"\
     | sudo tee /etc/apt/sources.list.d/unit.list
 
+    # Mark to reload the package list
     reload="enabled"
 fi
 
-# manage new system update if require
+# reload the package list if required
 if [[ "$reload" == "enabled" ]]; then
     sudo apt-get update -qq || exit 1; fi
 
@@ -102,7 +105,7 @@ if ! grep -q "export SWS_PYTHON_ENV=" ~/.bashrc; then
   source ~/.bashrc
 else
   echo "Sweetheart python env update in .bashrc"
-  sed -i "s|export SWS_PYTHON_ENV=.*|export SWS_PYTHON_ENV=$SWS_PYTHON_ENV|" ~/.bashrc
+  sed -i "s|^export SWS_PYTHON_ENV=.*|export SWS_PYTHON_ENV=$SWS_PYTHON_ENV|" ~/.bashrc
   # shellcheck disable=SC1090
   source ~/.bashrc
 fi

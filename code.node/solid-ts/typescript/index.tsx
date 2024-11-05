fbba1c7d3b104c776d1c62f0ba1adb94d2bb897b
Nicolas Champion <champion.nicolas@gmail.com>
2024-11-05 07:09:43 +0100

    new real-time table with solid

--- code.node/solid-ts/typescript/index.tsx
+++ code.node/solid-ts/typescript/index.tsx
@@ -1,24 +1,89 @@
 import '../resources/tailwind.css'
 import { render } from 'solid-js/web'
+import { For, Show, createResource } from 'solid-js'
 
-function App() {
+// create websocket connection:
 
-  fetch("/data",{
-    method: "GET",
-    headers: {"sweetheart-action":"fetch.test"} })
-  .then(response => response.json())
-  .then(data => { console.log(data) })
+const ws = new WebSocket("ws://localhost:8080/data")
+ws.onopen = () => {console.log('WebSocket connection open')}
+ws.onerror = () => {console.log('WebSocket connection error')}
+ws.onclose = () => {console.log('WebSocket connection closed')}
 
+// test connection to server:
+
+fetch("/data",{
+  headers: {"sweetheart-action":"fetch.test"}
+})
+.then(response => response.json())
+.then(data => { console.log(data) })
+
+// set data types:
+
+interface Row {
+  id: string
+  [field: string]: string | number }
+
+// fetch initial data:
+
+async function fetchInit(): Promise<Row[]> {
+  const response = await fetch("/data", {
+    headers: { "sweetheart-action": "fetch.init" }
+  })
+  return await response.json()
+}
+
+// set main component:
+
+function Table() {
+
+  const [data, { mutate }] = createResource<Row[]>(fetchInit)
+  
   return(
-    <div class="my-28">
+    <div>
+      <h1 class="text-4xl lg:text-6xl text-pink-500 italic my-4">
+        realtime table </h1>
 
-      <h1 class="text-4xl lg:text-6xl text-pink-500 text-center italic">
-        Welcome to Sweetheart </h1>
+      <Show when={data.loading}>
+        <div>loading...</div> </Show>
 
-      <p class="text-center mt-4">
-        get at {window.location.pathname} </p>
+      <Show when={data.error}>
+        <div>error: {data.error.message}</div> </Show>
+
+      <Show when={data()}>
+        {(data) => (
+          <table class="table-auto mx-auto border-collapse border">
+            <thead>
+              <tr>
+                <For each={Object.keys(data()[0])}>
+                  {(header) => (
+                    <th class="border px-4 py-2 bg-gray-100">
+                      {header}
+                    </th>
+                  )}
+                </For>
+              </tr>
+            </thead>
+            <tbody>
+              <For each={data()}>
+                {(row) => (
+                  <tr id={row.id}>
+                    <For each={Object.keys(data()[0])}>
+                      {(name) => (
+                        <td class="border px-4 py-2">
+                          <input type="text" name={name} value={row[name]} />
+                        </td>
+                      )}    
+                    </For>
+                  </tr>
+                )}
+              </For>
+            </tbody>
+          </table>
+        )}
+      </Show>
     </div>
   )
 }
 
-render(App,document.getElementById('webapp')!)
+// render app:
+render(() => <Table />,document.getElementById('webapp')!)

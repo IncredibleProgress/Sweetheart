a9c2c1742873d6e4475df7c593b9c92179d880ca
Nicolas Champion <champion.nicolas@gmail.com>
2024-11-21 21:46:53 +0100

    Update tabulator.tsx

--- code.node/solid-ts/typescript/tabulator.tsx
+++ code.node/solid-ts/typescript/tabulator.tsx
@@ -2,7 +2,6 @@ import { onMount } from 'solid-js'
 import { TabulatorFull } from 'tabulator-tables'
 import '../node_modules/tabulator-tables/dist/css/tabulator_simple.min.css'
 
-
 export interface DataRow {
   id: string | null
   [field: string]: string | number | null
@@ -26,26 +25,59 @@ Promise<DataRow[]> {
   return json
 }
 
+export const Tabulator = ()=> {
 
-export const Tabulator = () => {
-
+  // create reference (required):
   let tabulatorRef
-  // const [ hub, load ] = useDataHub("testtable")
-  // const [ data, { mutate, refetch } ] = createResource(load)
-  
+
+  // create websocket connection:
+  const ws = new WebSocket("http://127.0.0.1:8080/data")
+  ws.onopen = () => {console.log('WebSocket connection open')}
+  ws.onerror = () => {console.log('WebSocket connection error')}
+  ws.onclose = () => {console.log('WebSocket connection closed')}
+
+  // build custom real-time input editor:
+  function rtInput
+    (cell, onRendered, success, cancel, editorParams):
+    HTMLInputElement {
+
+    //create and style input
+    const input = document.createElement("input")
+    input.setAttribute("value", cell.getValue())
+    input.setAttribute("type", "text")
+    input.style.padding = "3px"
+
+    function onChange() {
+      ws.send(JSON.stringify({
+        // head
+        action: "ws.rest.patch",
+        target: "test.testtable",
+        // body
+        id: cell.getRow().getData().id,
+        name: cell.getField(),
+        value: input.value
+      }))
+    }
+
+    input.addEventListener("input", onChange)
+    input.addEventListener("blur", ()=> success(input.value))
+    return input
+  }
+
+  // create and mount tabulator table:
   onMount(() => {
-    let table = new TabulatorFull(
-      tabulatorRef, {
+    let table = new TabulatorFull( tabulatorRef,
+      {
         ajaxURL: "http://127.0.0.1:8080/data",
-        ajaxParams: {table:"testtable",database:"test"},
+        ajaxParams: { table:"testtable", database:"test" },
         ajaxRequestFunc: fetchInit,
         layout: "fitColumns",
         columns: [
-          { title:"Key", field:"key", editor:"input" },
-          { title:"Value", field:"value", editor:"input" }
+          { title:"Id", field:"id", visible:false },
+          { title:"Key", field:"key", editor: rtInput },
+          { title:"Value", field:"value", editor: rtInput }
         ]
       })
   })
-
   return <div ref={tabulatorRef}></div>
 }
